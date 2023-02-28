669b3acf850e36211718ad95a071eb6b7772c8b6
Katerine Witkoski <ka.lwitkoski@gmail.com>
2023-02-28 16:21:30 -0300

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JavaCourse
+
+Repositório de apoio as aulas de Programação e Soluções computacionais (UAM - Ciência da Computação) 
+
---
+++ src/javacourse/JavaCourse.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javacourse;
+
+/**
+ *
+ * @author katerinelindawitkoski
+ */
+public class JavaCourse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        // TODO code application logic here
+    }
+    
+}
