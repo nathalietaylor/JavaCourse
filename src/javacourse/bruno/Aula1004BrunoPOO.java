f34da8b74a5a0a7e2d1e4f6927cd1f2222c29469
Katerine Witkoski <ka.lwitkoski@gmail.com>
2023-04-10 10:47:26 -0300

    POO

--- src/javacourse/bruno/Aula1004BrunoPOO.java
+++ src/javacourse/bruno/Aula1004BrunoPOO.java
@@ -1,8 +1,26 @@
 package javacourse.bruno;
 
-/**
- *
- * @author katerinelindawitkoski
+/*
+Atributos - características: 
+- nome (string)
+- ra (integrer)
+- curso (string)
+- notas (a1, a2, a3) (double)
+- presença (booleano)
+
+Métodos - Ações: (fora para dentro do objeto)
+- criar aluno - criarAluno()
+    como argumentos/parâmetros:
+    - fornecer o seu nome (getNome)
+    - fornecer o seu RA (getRA)
+    - fornecer o curso (getCurso)
+- pegar as notas (getA1, setA1 (para A2 e A3 também))
+    - getNotas() - vai retornar A1, A2, A3
+- pegar as presenças - (getPresenca, setPresenca)
+- identificar se está aprovado ou não (getAprovadoouNao)
+
+
+ @author katerinelindawitkoski
  */
 public class Aula1004BrunoPOO {
 
